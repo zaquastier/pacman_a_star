9b11c0da04811cb49f9e1c7d773eb7a390372ea5
zaquastier <davidvaldiviadonoyan@gmail.com>
2024-07-12 17:22:22 +0200

    a star for prize

--- pacman.py
+++ pacman.py
@@ -26,23 +26,17 @@ class Game:
         self.load(map_path)
 
     def a_star(self, stdscr):
-
         while True:
-            path = self.path_to_prize()
-            path = path[:-1]
-            
-            for move in path:
+            path = self.path_to_prize()[1:]
+            for i, move in enumerate(path):
                 x, y = move[0], move[1]
-                self.print_map(stdscr)
 
-                if self.map[self.y + y, self.x + x] == '*':
-                        self.update_map(self.x + x, self.y + y)
-                        self.load_prize()
-                if self.map[self.y + y, self.x + x] == '-':
-                    self.update_map(self.x + x, self.y + y)
-                sleep(0.5)
+                self.print_map(stdscr, opt=str(self.x) + ', ' + str(self.y) + '  ' + str(x) + ', ' + str(y), offset=1)
 
-            self.print_map(stdscr)
+                if self.map[self.y + y, self.x + x] == self.prize:
+                    self.load_prize()
+                self.update_map(self.x + x, self.y + y)
+                sleep(0.2)
 
     def start(self, stdscr):
         self.update_user(stdscr)
@@ -78,7 +72,7 @@ class Game:
         self.map[y, x] = self.symbol
 
     def load_prize(self):
-        self.prize = '*'
+        self.prize = 'A'
         while True:
             x, y = np.random.randint(self.width), np.random.randint(self.height)
             if self.map[y, x] == '-':
@@ -100,20 +94,17 @@ class Game:
             open_list = self.remove_node(open_list, current_node)
             closed_list.append(current_node)
 
-            print(f"current node: {current_node}")
-            
             if current_node.x == self.prize_x and current_node.y == self.prize_y:
                 path = []
                 current = current_node
                 while current is not None:
                     path.append((current.move_x, current.move_y))
                     current = current.parent
-                return path
+                return path[::-1]
 
             children_node = self.adjacent_nodes(current_node)
 
             for child in children_node:
-                print(f'child: {child}')
                 if child in closed_list:
                     continue
                 child.g = current_node.g + 1
@@ -147,10 +138,13 @@ class Game:
         x, y = node.x, node.y
         if x > 0 and (self.map[y, x-1] == '-' or self.map[y, x-1] == self.prize):
             nodes.append(Node(parent=node, x=x-1, y=y, move_x=-1, move_y=0))
+
         if x < self.width and (self.map[y, x+1] == '-' or self.map[y, x+1] == self.prize):
             nodes.append(Node(parent=node, x=x+1, y=y, move_x=1, move_y=0))
+
         if y > 0 and (self.map[y-1, x] == '-' or self.map[y-1, x] == self.prize):
             nodes.append(Node(parent=node, x=x, y=y-1, move_x=0, move_y=-1))
+
         if y < self.height and (self.map[y+1, x] == '-' or self.map[y+1, x] == self.prize):
             nodes.append(Node(parent=node, x=x, y=y+1, move_x=0, move_y=1))
 
@@ -166,25 +160,25 @@ class Game:
             if user_input == 'p':
                 break
             elif user_input == 'q':
-                if x > 0 and self.map[y, x-1] == '*':
+                if x > 0 and self.map[y, x-1] == self.prize:
                     self.update_map(x-1, y)
                     self.load_prize()
                 if x > 0 and self.map[y, x-1] == '-':
                     self.update_map(x-1, y)
             elif user_input == 'd':
-                if x < self.width and self.map[y, x+1] == '*':
+                if x < self.width and self.map[y, x+1] == self.prize:
                     self.update_map(x+1, y)
                     self.load_prize()
                 if x < self.width and self.map[y, x+1] == '-':
                     self.update_map(x+1, y)
             elif user_input == 's':
-                if y < self.height and self.map[y+1, x] == '*':
+                if y < self.height and self.map[y+1, x] == self.prize:
                     self.load_prize()
                     self.update_map(x, y+1)
                 if y < self.height and self.map[y+1, x] == '-':
                     self.update_map(x, y+1)
             elif user_input == 'z':
-                if y > 0 and self.map[y-1, x] == '*':
+                if y > 0 and self.map[y-1, x] == self.prize:
                     self.load_prize()
                     self.update_map(x, y-1)
                 if y > 0 and self.map[y-1, x] == '-':
@@ -206,5 +200,5 @@ class Game:
         stdscr.refresh()
 
 if __name__ == '__main__':
-    game = Game('maps/simple_map.txt')
+    game = Game('maps/test_map.txt')
     curses.wrapper(game.a_star)
